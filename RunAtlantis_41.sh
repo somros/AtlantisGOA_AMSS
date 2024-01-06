80593d25d48333808c7cf204dfde959c6fc3a85f
somros <alberto.rovellini@gmail.com>
2024-01-05 18:27:42 -0800

    added files for AMSS runs

---
+++ RunAtlantis_41.sh
@@ -0,0 +1,1 @@
+atlantisMerged -i GOA_cb_summer.nc  0 -o output_41.nc -r GOA_run.prm -f GOA_force_warm.prm -p GOA_physics.prm -b GOAbioparam_test.prm -h GOA_harvest_f35_8_ATF.prm -m GOAMigrations.csv -s GOA_Groups.csv -q GOA_fisheries.csv -d output
---
+++ RunAtlantis_44.sh
@@ -0,0 +1,1 @@
+atlantisMerged -i GOA_cb_summer.nc  0 -o output_44.nc -r GOA_run.prm -f GOA_force_warm.prm -p GOA_physics.prm -b GOAbioparam_test.prm -h GOA_harvest_f35_11_ATF.prm -m GOAMigrations.csv -s GOA_Groups.csv -q GOA_fisheries.csv -d output
